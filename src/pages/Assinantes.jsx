919490ce4634b80fb96eff1e9616995077224e1b
Wesley Figueiredo <westtlley@gmail.com>
2026-01-18 00:06:42 -0300

    fix: corrigir tags JSX nÃ£o fechadas em Assinantes.jsx
    - Adicionar divs de fechamento faltantes
    - Corrigir estrutura de nested divs antes de motion.div

--- src/pages/Assinantes.jsx
+++ src/pages/Assinantes.jsx
@@ -1209,6 +1209,7 @@ export default function Assinantes() {
                         </DropdownMenuContent>
                       </DropdownMenu>
                     </div>
+                    </div>
                   </div>
                   </motion.div>
                 );
