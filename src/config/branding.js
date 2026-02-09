04d8e9c7197df7f3afbef2a9db122820534ad31a
westtlley <westtlley@gmail.com>
2026-02-09 18:05:08 -0300

    Update branding.js

--- src/config/branding.js
+++ src/config/branding.js
@@ -3,7 +3,7 @@
  * Altere aqui para trocar nome, logo e favicon em todo o app.
  * Mantenha index.html (title e link rel="icon") e vite.config.js (name/short_name) em sync quando mudar.
  */
-export const SYSTEM_NAME = 'WF-Menu';
-export const SYSTEM_NAME_SHORT = 'WF-Menu';
+export const SYSTEM_NAME = 'DigiMenu';
+export const SYSTEM_NAME_SHORT = 'DigiMenu';
 export const SYSTEM_LOGO_URL = '/images/digimenu-logo.png';
 export const SYSTEM_FAVICON_URL = '/images/digimenu-logo.png';
